a38345ee34992318954e2a438fadb8eabeb0da8c
Juan Pablo Etchart <114407900+juanetchart29@users.noreply.github.com>
2023-11-17 19:40:28 -0300

    fist steps with database

--- Etchart.JuanPablo/AccesoDatos.cs
+++ Etchart.JuanPablo/AccesoDatos.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace Entidades
 {
     internal class AccesoDatos
     {
+        private SqlConnection conexion;// se encarga de conectarse con el motor de la base de datos
+        private static string cadena_conexion;
+
+        static AccesoDatos()
+        {
+            AccesoDatos.cadena_conexion = 
+        }
     }
 }
